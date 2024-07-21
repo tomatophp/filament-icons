754c18b7448c45c763307d37f4062477ec02830b
Fady Mondy <info@3x1.io>
2024-07-21 18:17:10 +0300

    check if icon exists

--- resources/views/icon-column.blade.php
+++ resources/views/icon-column.blade.php
@@ -1,6 +1,15 @@
+@php
+    $iconExists = false;
+    try {
+        app(\BladeUI\Icons\Factory::class)->svg($getState());
+         $iconExists = true;
+    }catch (\Exception $e){}
+@endphp
+@if($iconExists)
 <div>
     @php $checkIconExists = \TomatoPHP\FilamentIcons\Models\Icon::where('name', $getState())->first() @endphp
     @if($checkIconExists)
         <x-icon :name="$getState()" class="h-6 w-6"  />
     @endif
 </div>
+@endif
